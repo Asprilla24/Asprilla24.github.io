d2b728d11c12972d8ccb9c59e732ba7040e99700
Jihan Arifia <jihanarifiar@gmail.com>
2023-07-04 21:49:00 +0700

    add change lang

--- js/i18n.js
+++ js/i18n.js
@@ -1,36 +1,163 @@
-function updateContent() {
-    const elements = document.getElementsByClassName("i18nelement");
-    for (let i = 0; i < elements.length; i++) {
-      const element = elements[i];
-      const k = element.getAttribute("data-i18n");
-      element.innerHTML = i18next.t(k);
-    }
-  }
-  
-  async function i18Loader() {
-    const langs = ["en", "id"];
-    const jsons = await Promise.all(
-      langs.map((l) => fetch("js/i18n/" + l + ".json").then((r) => r.json()))
-    );
-    const res = langs.reduce((acc, l, idx) => {
-      acc[l] = { translation: jsons[idx] };
-      return acc;
-    }, {});
-    await i18next.init({
-      lng: "en",
-      debug: true,
-      resources: res
-    });
-    updateContent();
-    i18next.on("languageChanged", () => {
-      updateContent();
-    });
-    const langSelector = document.getElementById("langSelector");
-    langSelector.removeAttribute("disabled");
-    langSelector.addEventListener("change", (e) => {
-      i18next.changeLanguage(e.target.value);
-    });
-  }
-  
-  i18Loader();
-  
+
+var dictionary = {
+  thankyou: {
+    id: "Terima Kasih",
+    en: "Thank you",
+  },
+  "thankyou-desc": {
+    id: "Merupakan suatu kehormatan dan kebanggaan bagi kami, apabila Bapak/Ibu/Saudara/i, berkenan hadir dan memberikan doa restu kepada kami.",
+    en: "Merupakan suatu kehormatan dan kebanggaan bagi kami, apabila Bapak/Ibu/Saudara/i, berkenan hadir dan memberikan doa restu kepada kami.",
+  },
+  submitAttending: {
+    id: "Saya Hadir",
+    en: "I'm Attending",
+  },
+  submitNotAttending: {
+    id: "Tidak Hadir",
+    en: "Not Attending",
+  },
+  'Are You Attending?': {
+    id: "Apakah Kamu Hadir?",
+    en: "Are You Attending?",
+  },
+  'Are You Attending? Desc': {
+    id: "Please Fill-up the form to notify you that you're attending. Thanks.",
+    en: "Please Fill-up the form to notify you that you're attending. Thanks.",
+  },
+  'Best Wishes': {
+    id: "Best Wishes",
+    en: "Best Wishes",
+  },
+  'Friends Wishes': {
+    id: "Friends Wishes",
+    en: "Friends Wishes",
+  },
+  openMapsHere: {
+    id: "Buka Google Maps Disini!",
+    en: "Open Google Maps Here!",
+  },
+  maps: {
+    id: "Maps",
+    en: "Peta",
+  },
+  weddinglocation: {
+    id: "Wedding Location",
+    en: "Lokasi Pernikahan",
+  },
+  memoriesDesc: {
+    id: "Cool pictures with peaky blinders and onepiece references, because we love it.",
+    en: "Cool pictures with peaky blinders and onepiece references, because we love it.",
+  },
+  'arrum-arti': {
+    id: "21. Dan di antara tanda-tanda (kebesaran)-Nya ialah Dia menciptakan pasangan-pasangan untukmu dari jenismu sendiri, agar kamu cenderung dan merasa tenteram kepadanya, dan Dia menjadikan di antaramu rasa kasih dan sayang. Sungguh, pada yang demikian itu benar-benar terdapat tanda-tanda (kebesaran Allah) bagi kaum yang berpikir.",
+    en: "21. And one of His signs is that He created for you spouses from among yourselves so that you may find comfort in them. And He has placed between you compassion and mercy. Surely in this are signs for people who reflect.",
+  },
+  'qs-arrum': {
+    id: "QS. Ar-Rum Ayat 21",
+    en: "Surah Ar-Rum Ayat 21",
+  },
+  'relationship-desc': {
+    id: "On the bustling streets of Malioboro, Jogja, time flies by as we share stories, laughs and vulnerabilities, feeling the undeniable chemistry between us. With each passing moment, we realized that this first encounter was extraordinary. It was the start of a deep and meaningful relationship filled with love, growth, and a shared journey towards an extraordinary future.",
+    en: "On the bustling streets of Malioboro, Jogja, time flies by as we share stories, laughs and vulnerabilities, feeling the undeniable chemistry between us. With each passing moment, we realized that this first encounter was extraordinary. It was the start of a deep and meaningful relationship filled with love, growth, and a shared journey towards an extraordinary future.",
+  },
+  'firstdate-desc': {
+    id: "As the sun began to set, illuminating the city of Malang warmly, we started our first date. Nervous joy filled the air as we sat around the same table and got to know each other on a deeper level. With each step, our relationship grew stronger, and the conversation flowed smoothly. It was the perfect first date, full of shared moments, genuine connection, and the promise of a beautiful future together.",
+    en: "As the sun began to set, illuminating the city of Malang warmly, we started our first date. Nervous joy filled the air as we sat around the same table and got to know each other on a deeper level. With each step, our relationship grew stronger, and the conversation flowed smoothly. It was the perfect first date, full of shared moments, genuine connection, and the promise of a beautiful future together.",
+  },
+  'firstmeet-desc': {
+    id: "Our paths crossed when we were both in the same class, RPL 4 (Serepet) at SMK Telkom Malang. Little did we know that this chance encounter would mark the beginning of an extraordinary friendship that would weather the highs and lows of high school, creating memories that will last a lifetime.",
+    en: "Our paths crossed when we were both in the same class, RPL 4 (Serepet) at SMK Telkom Malang. Little did we know that this chance encounter would mark the beginning of an extraordinary friendship that would weather the highs and lows of high school, creating memories that will last a lifetime.",
+  },
+  'ourstory-desc': {
+    id: "a little story of our journey together",
+    en: "a little story of our journey together",
+  },
+  'ourstory-title': {
+    id: "Our Story",
+    en: "Our Story",
+  },
+  gettingStarted: {
+    id: "We Are Getting Married",
+    en: "We Are Getting Married",
+  },
+  'we-invited': {
+    id: "We invited you to celebrate our wedding",
+    en: "We invited you to celebrate our wedding",
+  },
+  'wedding-date': {
+    id: "15 Juli 2023. Kepanjen, Malang",
+    en: "July 15th, 2023. Kepanjen, Malang",
+  },
+  'groom-desc': {
+    id: "an ordinary man who with the extraordinary gift of having friends and a future wife who is loyal, kind, patient and loves to joke around. Is the only child of a father and mother with countless struggles and services, you are the best!",
+    en: "an ordinary man who with the extraordinary gift of having friends and a future wife who is loyal, kind, patient and loves to joke around. Is the only child of a father and mother with countless struggles and services, you are the best!",
+  },
+  'bride-desc': {
+    id: "Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. Separated they live in Bookmarksgrove",
+    en: "Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. Separated they live in Bookmarksgrove",
+  },
+  'Our Special Events': {
+    id: "Our Special Events",
+    en: "Our Special Events",
+  },
+  'Wedding Events': {
+    id: "Wedding Events",
+    en: "Wedding Events",
+  },
+  'Akad Nikah': {
+    id: "Akad Nikah",
+    en: "Wedding Ceremony",
+  },
+  'Akad Nikah Desc': {
+    id: "",
+    en: "Marriage contract by the groom agreeing to wed the bride based on the Islamic requirement and rules.",
+  },
+  Saturday: {
+    id: "Sabtu",
+    en: "Saturday",
+  },
+  'Wedding Date': {
+    id: "15 Juli 2023",
+    en: "Juli 15th, 2023",
+  },
+  'Wedding Party': {
+    id: "Resepsi",
+    en: "Wedding Party",
+  },
+  'Wedding Party Desc': {
+    id: "",
+    en: "Ritual officially celebrating the beginning of marriage",
+  },
+  Saturday: {
+    id: "Sabtu",
+    en: "Saturday",
+  },
+  Saturday: {
+    id: "Sabtu",
+    en: "Saturday",
+  },
+};
+
+// maps: {
+//   id: "Maps",
+//   en: "Peta",
+// },
+// data-translate="submitAttending" 
+var langs = ["id", "en"];
+var current_lang_index = 0;
+var current_lang = langs[current_lang_index];
+
+window.change_lang = function () {
+  current_lang_index = ++current_lang_index % 2;
+  current_lang = langs[current_lang_index];
+  translate();
+};
+
+function translate() {
+  $("[data-translate]").each(function () {
+    var key = $(this).data("translate");
+    $(this).html(dictionary[key][current_lang] || "");
+  });
+}
+
+translate();
